83840848d95d5b66956d716d028072d364a0e2a2
Risinggirl <wlxyy@vip.qq.com>
2016-08-18 18:04:48 +0800

    Initial Commit

---
+++ Hongyunxiao_iOSTests/Hongyunxiao_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hongyunxiao_iOSTests.swift
+//  Hongyunxiao_iOSTests
+//
+//  Created by xiaocool on 16/8/18.
+//  Copyright © 2016年 xiaocool. All rights reserved.
+//
+
+import XCTest
+@testable import Hongyunxiao_iOS
+
+class Hongyunxiao_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
